a6fffaca1a8fd819fcf39b187036b02d8f036c4a
Emiel Kuiper <emielkuiper@gmail.com>
2018-09-03 11:48:18 +0200

    Added checks to demo script.

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
 
+###
+### Variables
+###
+
 ANSIBLE_INVENTORY="../vagrant/inventory/hosts"
-
 ZK_HOSTS=`ansible-inventory --list -i $ANSIBLE_INVENTORY | jq -r '.kafka_brokers[] | @sh'`
-
 FIRST_BROKER=`ansible-inventory --list -i $ANSIBLE_INVENTORY | jq -r '.kafka_brokers[] | [limit(1;.[])] | @sh' | sed s/\'//g`
-
+FIRST_WORKERNODE=`ansible-inventory --list -i $ANSIBLE_INVENTORY | jq -r '.kafka_worker_nodes[] | [limit(1;.[])] | @sh' | sed s/\'//g`
 TOPIC_NAME="test"
-
 TXT_FEED="./demo.txt"
 
-
-#echo $ZK_HOSTS | sed s/\ /\\n/g | sed s/\'//g | while read line 
-#do 
-# MODE=`echo stats | nc $line 2181 | grep Mode | sed s/Mode\:\ //g`
-# echo "Zookeeper host $line is running as: $MODE"
-#done
+PORT_BROKER="9092"
+PORT_REST_PROXY="8082"
+PORT_ZOOKEEPER="2181"
 
 
 ###
 ### Create and verify a test topic
 ###
 
-echo "Creating test topic: $TOPIC_NAME"
-echo "Talking to broker: $FIRST_BROKER"
+# Check if topic exists by checking the REST API on worker node
+TOPIC_EXISTS=`curl -s -XGET -H 'Accept: application/vnd.kafka.v2+json' 'http://$FIRST_WORKER_NODE:$PORT_REST_PROXY/topics/$TOPIC_NAME' | jq  '.message | @text'`
 
-kafka-topics --create --zookeeper $FIRST_BROKER:2181 --replication-factor 3 --partitions 1 --topic $TOPIC_NAME
+# Create topic if not exists
+if   [[ "$TOPIC_EXISTS" =~ "Topic not found" ]]; then
+    echo "OK: Topic $TOPIC_NAME does not exist yet."
+    echo "Creating test topic: $TOPIC_NAME"
+    echo "Talking to broker: $FIRST_BROKER"
+    kafka-topics --create --zookeeper $FIRST_BROKER:2181 --replication-factor 3 --partitions 1 --topic $TOPIC_NAME
+    sleep 2
+else
+    echo "Topic $TOPIC_NAME already exists. Not creating again."
+fi
 
-sleep 2
 
 
 ###
@@ -36,7 +42,7 @@ sleep 2
 
 echo "Verify topic"
 
-kafka-topics --zookeeper $FIRST_BROKER:2181 --describe --topic $TOPIC_NAME
+kafka-topics --zookeeper $FIRST_BROKER:$PORT_ZOOKEEPER --describe --topic $TOPIC_NAME
 
 sleep 2
 
@@ -45,20 +51,29 @@ sleep 2
 ### Consume messages from a test topic:
 ###
 
-# copy paste the following line as a whole:
 
-echo "Starting consumer process in the background...."
+# Check if consumer has already been started by checking local process list
+CONSUMER_EXISTS=`ps aux | grep "ConsoleConsumer"`
 
-kafka-console-consumer --bootstrap-server $FIRST_BROKER:9092 --topic $TOPIC_NAME --from-beginning &
-
+# Start consumer if not already running
+if   [[ "$CONSUMER_EXISTS" =~ "ConsoleConsumer --bootstrap-server $FIRST_BROKER:$PORT_BROKER --topic $TOPIC_NAME" ]]; then
+    echo "Consumer already running. Not starting again."
+else
+    echo "OK: Consumer not yet running."
+    echo "Starting consumer process in the background. Talking to broker: $FIRST_BROKER"
+    kafka-console-consumer --bootstrap-server $FIRST_BROKER:$PORT_BROKER --topic $TOPIC_NAME --from-beginning &
+    sleep 2
+fi
 
 
 ###
 ### Produce messages to a test topic:
 ###
 
-echo "Sleeping for 3 seconds... any messages you see scrolling by have been processed correctly by Kafka..."
+echo "Going to push 10000 test messages to Kafka.."
+echo "Any messages you see scrolling by have been processed correctly by Kafka..."
+echo "Sleeping for 3 seconds..."
 sleep 3
-cat $TXT_FEED | kafka-console-producer --broker-list $FIRST_BROKER:9092 --topic $TOPIC_NAME
+cat $TXT_FEED | kafka-console-producer --broker-list $FIRST_BROKER:$PORT_BROKER --topic $TOPIC_NAME
 
 
